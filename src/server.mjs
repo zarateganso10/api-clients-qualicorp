dffed181baf9450dad0c829d9806aa4a9feb4ecb
zarateganso10 <matheuszarate55@gmail.com>
2022-07-07 08:48:33 -0300

    add a default port api

--- src/server.mjs
+++ src/server.mjs
@@ -1,3 +1,3 @@
 import { app } from './app.mjs';
 
-app.listen(process.env.PORT, () => console.log(`api listening http://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`api listening http://localhost:${process.env.PORT || 3333}`));
